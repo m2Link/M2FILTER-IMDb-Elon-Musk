6f819c9ba1e67abea85fd4ade7cfcf5854cccf97
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-30 23:25:24 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -94,7 +94,7 @@ def get_reply_markup(query):
             InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
         ],
         [
-            InlineKeyboardButton('Group ♻️', url='https://t.me/m_Discussion_Group)
+            InlineKeyboardButton('Group ♻️', url='https://t.me/m_Discussion_Group')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
